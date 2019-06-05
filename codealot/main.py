936c00c09ceb167ef35970cf82de29ff0e8fc027
Arnav Kaul <akkaul1195@gmail.com>
2019-06-04 22:32:09 -0500

    Last Email: Added a King Arthur flag in the Knight class, which gets set on initialization, and I add him first to the initial list before iterating through the 12 other knights, King Arthur Flag is used to separate the other knights total XP gained at the end of the period. I also added a new flag, and set/get functions for the Round Table, and created an increment Round Table function that adds to a counter. At the end of the Day I check if the counter is above or equal to 3 at the RT, and if not, no XP is counted for the day. I also had to add in an additional if/else statement for the Stamina as we do not decrease Stamina at the round table. At the end of each day, I reset the counter where I reset the Stamina flag from the fourth email. Finally, I print out the 12 Knights stats, and on another line I Print out King Arthurs stats, for the whole period.

--- codealot/main.py
+++ codealot/main.py
@@ -4,12 +4,15 @@ import sys
 class Knight(object):
     __isInTavern = False
     __isInTrainingYard = False
+    __isInRoundTable = False
     __staminaStillValid = True
+    __RoundTableCount = 0
 
-    def __init__(self):
+    def __init__(self, KA):
         self.__totalxp = 0
         self.__xp = 0
         self.__stamina = 0
+        self.__KingArthur = KA
 
     def getXp(self):
         return self.__xp
@@ -34,11 +37,20 @@ class Knight(object):
         if self.__stamina < 0:
             self.__staminaStillValid = False
 
+    def incrementRT(self, val):
+        self.__RoundTableCount += val
+
     def isInTavern(self):
         return self.__isInTavern
 
     def setInTavern(self, isInTavern):
         self.__isInTavern = isInTavern
+
+    def isInRoundTable(self):
+        return self.__isInRoundTable
+
+    def setInRoundTable(self, isInRoundTable):
+        self.__isInRoundTable = isInRoundTable
 
     def isInTrainingYard(self):
         return self.__isInTrainingYard
@@ -47,12 +59,17 @@ class Knight(object):
         self.__isInTrainingYard = isInTrainingYard
 
     def addXpToTotal(self):
-        self.__totalxp += self.__xp
+        if self.__RoundTableCount >= 3:
+            self.__totalxp += self.__xp
         self.__xp = 0
         self.__staminaStillValid = True
+        self.__RoundTableCount = 0
 
     def getTotalXp(self):
         return self.__totalxp
+
+    def isKingArthur(self):
+        return self.__KingArthur
 
 class Codalot(object):
     knights = []
@@ -67,16 +84,25 @@ class Codalot(object):
         self.knights.append(knight)
         knight.setInTrainingYard(True)
         knight.setInTavern(False)
+        knight.setInRoundTable(False)
 
     def addKnightToTavern(self, knight):
         self.knights.append(knight)
         knight.setInTavern(True)
         knight.setInTrainingYard(False)
+        knight.setInRoundTable(False)
+
+    def addKnightToRoundTable(self, knight):
+        self.knights.append(knight)
+        knight.setInRoundTable(True)
+        knight.setInTavern(False)
+        knight.setInTrainingYard(False)
 
     def process(self):
         for knight in self.knights:
-            knight.incrementStamina(1 if knight.isInTavern() else -1)
+            knight.incrementStamina(1 if knight.isInTavern() else 0 if knight.isInRoundTable() else -1)
             knight.incrementXp(1 if knight.isInTrainingYard() and knight.getStamina() > 0 else 0)
+            knight.incrementRT(1 if knight.isInRoundTable() else 0)
 
     def grantBonusXp(self):
         bonusKnights = 0
@@ -121,27 +147,34 @@ if __name__ == "__main__":
     codalot = Codalot()
 
     knights = list()
+    knights.append(Knight(True))
     for i in range(12):
-        knights.append(Knight())
+        knights.append(Knight(False))
 
     random = Random(1)
     for j in range(days):
         for i in range(24):
             codalot.clearKnights()
             for knight in knights:
-                randomVal = random.randint(0, 1)
+                randomVal = random.randint(0, 2)
                 if randomVal == 0:
                     codalot.addKnightToTrainingYard(knight)
                 elif randomVal == 1:
                     codalot.addKnightToTavern(knight)
+                elif randomVal == 2:
+                    codalot.addKnightToRoundTable(knight)
             codalot.process()
         codalot.grantBonusXp()
         codalot.addDailyXpToTotal()
 
     totalXp = 0
     for knight in knights:
-        totalXp = totalXp + knight.getTotalXp()
+        if knight.isKingArthur():
+            KingXP = knight.getTotalXp()
+        else:
+            totalXp += knight.getTotalXp()
 
-    print "Total XP earned by all " + str(len(knights)) + " knights in " + str(days) + " day(s): " + str(totalXp) 
+    print "Total XP earned by all " + str(len(knights)-1) + " knights in " + str(days) + " day(s): " + str(totalXp) 
+    print "Total XP earned by King Arthur in " + str(days) + " day(s): " + str(KingXP)
 
 
